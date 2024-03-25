686e1e965dfc70d052f7d03b4a5cc7b680c89cb6
GauravGupta993 <gaurav115993@gmail.com>
2024-03-25 19:17:51 +0530

    Removed cron to solve deployment issues.

--- events/ready.js
+++ events/ready.js
@@ -2,7 +2,6 @@ const { Events } = require('discord.js');
 const { CronJob } = require('cron');
 const Solvecheck = require('../utils/Solvecheck');
 const resetDaily = require('../utils/resetDaily');
-const schedule = require('node-schedule');
 const daily = require('../utils/daily');
 module.exports = {
 	name: Events.ClientReady,
@@ -12,6 +11,9 @@ module.exports = {
 		// setInterval(async function() {
 		// 	await daily(client);
 		// }, 60 * 1000); 
+		setInterval(async function () {
+			console.log("1 hour scheduler.");
+		}, 60 * 1000);
 		setInterval(async function () {
 			const h = new Date().getHours();
 			if (h == 0) {
@@ -24,45 +26,6 @@ module.exports = {
 		setInterval(async function () {
 			await daily(client);
 		}, 60 * 60 * 4 * 1000);
-		// const test = schedule.scheduleJob('0 * * * * *', async function () {
-		// 	await daily(client);
-		// });
-		// const job = schedule.scheduleJob('0 0 */6 * * *', async function () {
-		// 	await Solvecheck();
-		// });
-		// const job2 = schedule.scheduleJob('0 0 0 * * *', async function () {
-		// 	await resetDaily();
-		// });
-		// const job3 = schedule.scheduleJob('0 0 */12 * * *', async function () {
-		// 	await daily(client);
-		// });
-		// const job = new CronJob(
-		// 	'0 0 */6 * * *', // cronTime
-		// 	async function () {
-		// 		await Solvecheck();
-		// 	}, // onTick
-		// 	null, // onComplete
-		// 	true, // start
-		// 	'Asia/Kolkata' // timeZone
-		// );
-		// const job3 = new CronJob(
-		// 	'0 0 0 * * *', // cronTime
-		// 	async function () {
-		// 		await resetDaily();
-		// 	}, // onTick
-		// 	null, // onComplete
-		// 	true, // start
-		// 	'Asia/Kolkata' // timeZone
-		// );
-		// const job2 = new CronJob(
-		// 	'0 0 */12 * * *', // cronTime
-		// 	async function () {
-		// 		await daily(client);
-		// 	}, // onTick
-		// 	null, // onComplete
-		// 	true, // start
-		// 	'Asia/Kolkata' // timeZone
-		// );
 		console.log("Finish");
 
 	},
